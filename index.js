5a01c73b0dc3da96c92281e9bb8eb6316faf2519
Comfort Ige <eligenius@gmail.com>
2023-06-01 00:06:44 +0100

    Import important functions from Firebase

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+
+const appSettings = {
+  databaseURL: "https://simple-endorsements-default-rtdb.firebaseio.com/"
+}
