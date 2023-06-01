978c37cbed63c2a60d18156bbae98302fd6f6ced
Comfort Ige <eligenius@gmail.com>
2023-06-01 20:33:12 +0100

    Add event listener for button

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
   databaseURL: "https://simple-endorsements-default-rtdb.firebaseio.com/"
 }
+const messageEl = document.getElementById("message")
+const sendBtnEl = document.getElementById("send-btn")
+const endorsementListEl = document.getElementById("endorsement-list")
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsInDB = ref(database, "endorsements")
+
+sendBtnEl.addEventListener("click", ()=>{
+  let messageValue = messageEl.value
+  endorsementListEl.innerHTML += `<li>${messageValue}</li>`
+})
