d40f1c9b9dafeea8aca4ee7fde44bcb75fce2cd5
Comfort Ige <eligenius@gmail.com>
2023-06-02 17:26:04 +0100

    Fetch sender and receiver to be displayed on endorsementListEl

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com
 const appSettings = {
   databaseURL: "https://simple-endorsements-default-rtdb.firebaseio.com/"
 }
-const inputFieldEl = document.getElementById("message")
+const messageEl = document.getElementById("message")
+const senderEl = document.getElementById("from")
+const receiverEl = document.getElementById("to")
 const sendBtnEl = document.getElementById("send-btn")
 const endorsementListEl = document.getElementById("endorsement-list")
 
@@ -14,11 +16,13 @@ const database = getDatabase(app)
 const endorsementsInDB = ref(database, "endorsementList")
 
 sendBtnEl.addEventListener("click", ()=>{
-  let inputValue = inputFieldEl.value
-  if (inputFieldEl.value) {
-    clearinputFieldEl()
-    // addEndorsementsToListEl(inputFieldValue)
-    push(endorsementsInDB, inputValue)
+  let messageValue = messageEl.value
+  let senderValue = senderEl.value
+  let receiverValue = receiverEl.value
+  if (messageEl.value) {
+    clearmessageEl()
+    // addEndorsementsToListEl(messageValue)
+    push(endorsementsInDB, [messageValue, senderValue, receiverValue])
   }
   
 })
@@ -27,8 +31,10 @@ function clearListEl(){
   endorsementListEl.innerHTML = ""
 }
 
-function clearinputFieldEl(){
-  inputFieldEl.value = ""
+function clearmessageEl(){
+  messageEl.value = ""
+  senderEl.value = ""
+  receiverEl.value = ""
 }
 
 onValue(endorsementsInDB, (snapshot)=>{
@@ -47,15 +53,26 @@ onValue(endorsementsInDB, (snapshot)=>{
  
 })
 
-function appendToListEl(itemArr){
-  let itemID = itemArr[0]
-  let itemValue = itemArr[1]
+function appendToListEl(messageArr){
+  let messageID = messageArr[0]
+  let messageValue = messageArr[1][0]
+  let messageSender = messageArr[1][1]
+  let messageReceiver = messageArr[1][2]
 
-  const newEl = document.createElement("li")
-  newEl.textContent = itemValue
+  // const newEl = document.createElement("li")
+  // newEl.textContent = `${messageValue} <br>      from <strong>${messageSender}</strong>`
 
-  endorsementListEl.append(newEl)
-  // endorsementListEl.innerHTML += `<li>${itemValue}</li>`
+  // endorsementListEl.append(newEl)
+
+  endorsementListEl.innerHTML += 
+    `
+      <li>
+        to <strong>${messageReceiver}</strong> 
+        <br><br>
+        ${messageValue} <br><br> 
+        from <strong>${messageSender}</strong>
+      </li>
+    `
 
   newEl.addEventListener("click", ()=>{
     let locationInDB = ref(database, `endorsementList/${itemID}`)
